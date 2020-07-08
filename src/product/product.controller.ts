e6df44bf4f410041f459d961c98dce0ab155d3ba
CORTC068 <Cristian.X.Cortes.-ND@disney.com>
2020-07-08 08:47:21 -0500

    products service

---
+++ src/product/product.controller.ts
@@ -0,0 +1,59 @@
+import { Controller, Get, Post, Res, HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
+
+@Controller('products')
+export class ProductController {
+    @Get()
+    products(@Res() res: Response) {
+        res.status(HttpStatus.OK).json([
+            {
+                _id: 1,
+                image: 'hamburguesa.png',
+                name: 'Product 1',
+                price: '1000',
+                category: '1',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
+            },
+            {
+                _id: 2,
+                image: 'hamburguesa.png',
+                name: 'Product 2',
+                price: '2000',
+                category: '1',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
+            },
+            {
+                _id: 3,
+                image: 'hamburguesa.png',
+                name: 'Product 3',
+                price: '3000',
+                category: '1',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
+            },
+            {
+                _id: 4,
+                image: 'hamburguesa.png',
+                name: 'Product 4',
+                price: '4000',
+                category: '1',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
+            },
+            {
+                _id: 5,
+                image: 'hamburguesa.png',
+                name: 'Product 5',
+                price: '5000',
+                category: '1',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
+            },
+            {
+                _id: 6,
+                image: 'hamburguesa.png',
+                name: 'Product 6',
+                price: '6000',
+                category: '1',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
+            }
+        ]);
+    }
+}
